4a3d8e3b27fc39f1393f231b79178a59afbca577
hadiascripte <hadiascripte@gmail.com>
2025-03-07 15:29:03 +0100

    Opérateurs basique /String/ If Else

--- starter/script.js
+++ starter/script.js
@@ -26,7 +26,6 @@ let job1 = "programmer";
 let job2 = "teacher";
 */
 
-
 /*
 
 // 2. Data Types
@@ -54,9 +53,9 @@ console.log(typeof null); // C'est un bug de JavaScript, null est considéré co
 
 */
 
-
+/*
 // 3. Let, const and var
-// on utilise let pour les variables qui peuvent être modifiées plus tard, ou qui sont réassignées en les "mutants" (mutating) 
+// on utilise let pour les variables qui peuvent être modifiées plus tard, ou qui sont réassignées en les "mutants" (mutating)
 let age = 30;
 age = 31;
 
@@ -72,5 +71,125 @@ const birthYear = 1991;
 var job = "programmer";
 job = "teacher";
 
-lastName = "Schmedtmann";
-console.log(lastName);
+// lastName = "Schmedtmann";
+// console.log(lastName);
+*/
+
+// 4. Basic Operators
+
+/*
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+// x--;
+console.log(x);
+
+// Comparison operators
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+/*
+// 5. Operator Precedence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+/*
+// Challenge 1
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+*/
+
+/*
+// 6. Strings and Template Literals
+
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991; 
+const year = 2037;
+
+const jonas = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job} !`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`) ;
+
+*/
+
+// 7. Taking Decisions: if / else Statements
+
+/*
+const age = 15;
+// const isOldEnough = age >= 18;
+
+if(age>=18) {
+    console.log("Sarah can start driving license 🚗");
+}
+else{
+      const yearsleft = 18 - age;
+      console.log(`Sarah is too young. Wait another ${yearsleft} years :)`); 
+    } 
+
+const birthYear =2012;
+let century;
+if(birthYear <= 2000){
+century = 20;
+    console.log("You are a millenial");
+} else {
+      century = 21;
+    console.log("You are not a millenial");
+}
+console.log(century);
+*/
